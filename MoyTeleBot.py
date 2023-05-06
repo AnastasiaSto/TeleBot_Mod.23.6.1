c796f1da2697560e9cdb54170bbea225465fb03a
AnastasiaSto <110427579+AnastasiaSto@users.noreply.github.com>
2023-05-06 23:18:19 +0300

    Add files via upload

---
+++ MoyTeleBot.py
@@ -0,0 +1,43 @@
+import telebot
+from extensions import APIException, Convertor
+from config import TOKEN, exchanges
+import traceback
+
+exchanges = {
+    'доллар США': 'USD',
+    'евро': 'EUR',
+    'рубль РФ': 'RUB'
+}
+TOKEN = "1596902621:AAEnMFCBOg02PDigS5fAERfgcdKYsuNwb-8"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def start(message: telebot.types.Message):
+    text = "Приветствую"
+    bot.send_message(message.chat.id, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for i in exchanges.keys():
+        text = '\n'.join((text, i))
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text'])
+def converter(message: telebot.types.Message):
+    values = message.text.split(' ')
+    try:
+        if len(values) != 3:
+            raise APIException('Должно быть три параметра')
+        
+        answer = Convertor.get_price(*values)
+    except APIException as e:
+        bot.reply_to(message, f"Ошибка в команде:\n{e}" )
+    except Exception as e:
+        traceback.print_tb(e.__traceback__)
+        bot.reply_to(message, f"Неизвестная ошибка:\n{e}" )
+    else:
+        bot.reply_to(message,answer)
+
+bot.polling()
---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = "1444339635:AAEWaQQmfy-Guk47iioxwIDiT7lbL5h5z-I"
+exchanges = {
+    'доллар США': 'USD',
+    'евро': 'EUR',
+    'рубль РФ': 'RUB'
+}
